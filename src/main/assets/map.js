9ab8b67d464c855e1eaed1e7d056518f7bf7a35e
Hamilton <d63hbz@gmail.com>
2017-08-08 10:15:02 +0800

    [GoogleMap] UI modify
    Signed-off-by: Hamilton <d63hbz@gmail.com>

--- src/main/assets/map.js
+++ src/main/assets/map.js
@@ -117,3 +117,62 @@ function searchTxt(txt) {
 
 	service.textSearch(request, callback);
 }
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+var getLatLng = function(callback) {
+	var res = {lat: 39.903333, lng: 116.391667};
+	var lat = parseFloat(getParameterByName('lat'));
+	var lon = parseFloat(getParameterByName('lon'));
+  	if(lat && lon) {
+  		res = { lat: lat, lng: lon };
+  		if(callback)  callback(res);
+  	}
+  	else{
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+        	navigator.geolocation.getCurrentPosition(function(position) {
+        		res = {
+        			lat: position.coords.latitude,
+        			lng: position.coords.longitude
+        		};
+        		if(callback)  callback(res);
+        	}, function(error) {
+        		locationError(error, infoWindow, map.getCenter());
+        	}, {timeout:30000});
+        } else {
+          locationError(error, infoWindow, map.getCenter());
+        }
+  }
+  
+
+  return res;
+}
+
+function degrees_to_radians(degrees)
+{
+  var pi = Math.PI;
+  return degrees * (pi/180);
+}
+
+function distance(lat_a, lng_a, lat_b, lng_b) 
+{
+    var earthRadius = 6371.393;
+    var latDiff = degrees_to_radians(lat_b-lat_a);
+    var lngDiff = degrees_to_radians(lng_b-lng_a);
+    var a = Math.sin(latDiff /2) * Math.sin(latDiff /2) +
+    Math.cos(degrees_to_radians(lat_a)) * Math.cos(degrees_to_radians(lat_b)) *
+    Math.sin(lngDiff /2) * Math.sin(lngDiff /2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var distance = earthRadius * c;
+
+    return distance;
+}
